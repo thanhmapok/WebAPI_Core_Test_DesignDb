c7fa405b097cd3bc204c8256022e49b2f9ece20f
thanhmapok <thanhmapok@gmail.com>
2023-06-21 18:00:55 +0700

    Thêm HomeController

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_Core_Test_DesignDb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
